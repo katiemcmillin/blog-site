b787272b820b920494f19d03b3bdd49dda5a16ae
Katie McMillin <katiemcmillin@Katies-MBP.home>
2021-05-10 10:58:37 -0400

    get request successful

--- post-it/src/App.js
+++ post-it/src/App.js
@@ -1,18 +1,33 @@
-import './App.css';
+import "./App.css";
+import axios from "axios";
 import Nav from "./components/Nav";
 import { Route } from "react-router-dom";
-import Homepage from "./components/Homepage"
-import NewPage from "./components/NewPage"
+import Homepage from "./components/Homepage";
+import NewPage from "./components/NewPage";
+import PostsOfTheDay from "./components/PostOfTheDay"
+import { useEffect } from "react";
+import { baseURL, config } from "./services";
 
 function App() {
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await axios.get(baseURL, config);
+      console.log(response.data.records)
+    }
+    fetchPosts();
+}, [])
+
   return (
     <div className="App">
       <Nav />
-      <Route exact path='/'>
+      <Route exact path="/">
         <Homepage />
       </Route>
       <Route exact path="/new">
         <NewPage />
+      </Route>
+      <Route exact path="/posts-of-the-day">
+        <PostsOfTheDay />
       </Route>
     </div>
   );
